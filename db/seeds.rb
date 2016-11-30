6c4b87b769ab4e93e06e9ce1ebf3bf16a1d1c19d
JordanSaid <jordan_said@hotmail.co.uk>
2016-11-30 17:21:11 +0000

    added some extra seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,6 +38,30 @@ restaurant3 = Restaurant.new({
 
 restaurant3.save
 
+restaurant4 = Restaurant.new({
+  "name" => "Belted Burgers",
+  "website" => "http://www.beltedburgers.co.uk/belted-edinburgh",
+  "menu" => "http://www.beltedburgers.co.uk/pdf/edin_main_menu.pdf"
+  })
+
+restaurant4.save
+
+restaurant5 = Restaurant.new({
+  "name" => "The Other Place",
+  "website" => "http://www.theotherplaceedinburgh.com/",
+  "menu" => "http://www.theotherplaceedinburgh.com/images/top-main-menu.pdf"
+  })
+
+restaurant5.save
+
+restaurant6 = Restaurant.new({
+  "name" => "The City Cafe",
+  "website" => "http://citycafeedinburgh.co.uk/",
+  "menu" => "http://citycafeedinburgh.co.uk/main-menu/"
+  })
+
+restaurant5.save
+
 burger1 = Burger.new({
   "name" => "Cheeseburger",
   "restaurant_id" => restaurant1.id
@@ -93,6 +117,38 @@ deal3 = Deal.new({
 
 deal3.save
 
+deal4 = Deal.new({
+  "name" => "Burger & Beer/Wine £10",
+  "description" => "Available for all burger and selected drinks. Have to join burger society",
+  "day_id" => day3.id,
+  "restaurant_id" => restaurant4.id
+  })
+
+deal4.save
+
+deal5 = Deal.new({
+  "name" => "The Other Challenge",
+  "description" => "The Challenge is to eat the triple chilli cheese burger with a side of chilli cheese fries and onions rings within a 20 minute time limit.
+
+  The Challenge will cost £20, but if the challenger cleans the entire plate within the time limit, the meal and any drinks they have had during the challenge will be wiped from the bill.
+
+  The successful challenger will also have his/her picture put up on the wall of fame.",
+  "day_id" => day8.id,
+  "restaurant_id" => restaurant5.id
+  })
+
+deal5.save
+
+deal6 = Deal.new({
+  "name" => "Ultimate Burger Challenge",
+  "description" => "The Ultimate Burger has 3 homemade hamburgers, 2 marinated chicken breasts, 2 spicy bean burgers, 2 pineapple rings, bacon, mushrooms, gherkins, two types of cheese, guacamole, salad and more… all between a plate sized bun and served with a large portion of gourmet chips and coleslaw.
+
+  The Ultimate Burger Challenge is £30.00. If you finish it all, you get it for FREE, as well as a t-shirt and your photo on the wall of fame!",
+  "day_id" => day8.id,
+  "restaurant_id" => restaurant6.id
+  })
+
+deal5.save
 
 restaurant_deal1 = RestaurantDeal.new({
   "burger_id" => burger1.id,
